15054e6431b4408d09736f1311c72bb0ee6c0ae2
Marius Guggenmos <marius@quantstamp.com>
2024-02-12 10:19:11 +0100

    put colon on thumb instead of dot

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -47,7 +47,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 KC_5,              KC_6,              KC_7,              KC_8,              KC_9,              U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
 KC_0,              KC_1,              KC_2,              KC_3,              KC_4,              U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
 KC_GRV,            KC_SCLN,           KC_LBRC,           KC_RBRC,           KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
-U_NP,              U_NP,              KC_DOT,            KC_MINS,           KC_EQL,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              KC_COLN,           KC_MINS,           KC_EQL,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 // =======   SYMBOLS   =========
 #define MIRYOKU_ALTERNATIVES_SYM \
